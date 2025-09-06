5a1956db746712d20ca64c3acc0cd003292f49ba
affanahmed128 <affanahmed0128@gmail.com>
2025-09-06 17:51:57 +0530

    python test

--- test_main.py
+++ test_main.py
@@ -3,7 +3,7 @@ from main import add
 class TestMain(unittest.TestCase):
     def test_add(self):
         assert add(2,3) == 5
-        assert add(-1,2) == 0
+        assert add(-1,1) == 0
         
 # if expected output and actual output is same then we will get the result as pass        
 #Assert is Assertion  function which checking
