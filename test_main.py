4f38d6d21f0906c1509ceaa4645156bdeef3ae73
affanahmed128 <affanahmed0128@gmail.com>
2025-09-06 17:48:09 +0530

    python test

---
+++ test_main.py
@@ -0,0 +1,9 @@
+import unittest
+from main import add
+class TestMain(unittest.TestCase):
+    def test_add(self):
+        assert add(2,3) == 5
+        assert add(-1,2) == 0
+        
+# if expected output and actual output is same then we will get the result as pass        
+#Assert is Assertion  function which checking
